5b6c17ebb12ae2ee0411b335ac2101f5a604c039
FredDlt <fredy.delatorre.frd@gmail.com>
2020-10-31 21:55:26 -0500

    Primero commit

---
+++ app/src/main/java/com/example/s08_uso_sensores/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.s08_uso_sensores
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executor
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+                object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationError(errorCode: Int,
+                                                       errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        Toast.makeText(applicationContext,
+                                "Authentication error: $errString", Toast.LENGTH_SHORT)
+                                .show()
+                    }
+
+                    override fun onAuthenticationSucceeded(
+                            result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        Toast.makeText(applicationContext,
+                                "Authentication succeeded!", Toast.LENGTH_SHORT)
+                                .show()
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        Toast.makeText(applicationContext, "Authentication failed",
+                                Toast.LENGTH_SHORT)
+                                .show()
+                    }
+                })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setSubtitle("coloca tu huella en el sensor")
+                .setNegativeButtonText("Usar: usuario y contraseña")
+                .build()
+        ingresar_huella.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+    }
+}
